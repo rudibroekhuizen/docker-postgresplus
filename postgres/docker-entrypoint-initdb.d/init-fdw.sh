c84fbfa924ca04a6d88c240d279861a3441f79b7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-09-19 14:59:45 +0200

    Create init-fdw.sh

---
+++ postgres/docker-entrypoint-initdb.d/init-fdw.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE EXTENSION file_fdw;
+    CREATE SERVER pglog FOREIGN DATA WRAPPER file_fdw;
+    CREATE FOREIGN TABLE pglog (
+    log_time timestamp(3) with time zone,
+    user_name text,
+    database_name text,
+    process_id integer,
+    connection_from text,
+    session_id text,
+    session_line_num bigint,
+    command_tag text,
+    session_start_time timestamp with time zone,
+    virtual_transaction_id text,
+    transaction_id bigint,
+    error_severity text,
+    sql_state_code text,
+    message text,
+    detail text,
+    hint text,
+    internal_query text,
+    internal_query_pos integer,
+    context text,
+    query text,
+    query_pos integer,
+    location text,
+    application_name text
+    ) SERVER pglog
+    OPTIONS ( filename '/tmp/pglog.csv', format 'csv' );
+EOSQL
