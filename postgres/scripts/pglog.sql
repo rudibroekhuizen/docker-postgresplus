1126b704c6b7177cd721e2140d4142052ee8be40
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-09-25 13:16:59 +0200

    Update pglog.sql

--- postgres/scripts/pglog.sql
+++ postgres/scripts/pglog.sql
@@ -1,3 +1,5 @@
+-- yes "psql -t -f pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
+
 SELECT row_to_json(t)
 from (
 SELECT * from pglog WHERE log_time > current_timestamp - interval '1 minutes'
