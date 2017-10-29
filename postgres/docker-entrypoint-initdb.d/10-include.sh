802ce846de8243a1ee7651cfb8558310addda3de
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-10-29 19:44:53 +0100

    Update 10-include.sh

--- postgres/docker-entrypoint-initdb.d/10-include.sh
+++ postgres/docker-entrypoint-initdb.d/10-include.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
+#/bin/bash
+set -e
+
+# Override settings
 echo "include_dir = '/conf.d'" >> /pgdata/postgresql.conf
+
+# Create pg_hba entry for replication
+echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
+
+# Create replication user
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+     CREATE ROLE $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
+EOSQL
