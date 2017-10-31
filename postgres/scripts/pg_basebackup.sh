852d77ce8ecebee4a14cf7ba710311e34ee19d1e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-10-31 15:55:05 +0100

    Update pg_basebackup.sh

--- postgres/scripts/pg_basebackup.sh
+++ postgres/scripts/pg_basebackup.sh
@@ -8,3 +8,16 @@ pg_basebackup \
   --progress \
   --gzip \
   --xlog-method=fetch
+
+# To unpack:
+# gunzip base.tar.gz && tar -xvf base.tar
+
+
+
+# Stream method:
+#pg_basebackup \
+#  --host=127.0.0.1 \
+#  --username=$REPLICATION_USER \
+#  --pgdata=/tmp/backup \
+#  --progress \
+#  --xlog-method=stream
