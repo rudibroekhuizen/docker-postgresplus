2c91422df308c6aea806f1a91c20a3702e0499a2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-02 13:53:02 +0100

    Update 10-init.sh

--- postgres/docker-entrypoint-initdb.d/10-init.sh
+++ postgres/docker-entrypoint-initdb.d/10-init.sh
@@ -11,3 +11,6 @@ echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.con
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
      CREATE ROLE $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
 EOSQL
+
+# Start collecting logs
+yes "psql -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
