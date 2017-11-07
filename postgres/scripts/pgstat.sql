78a4a24d5193428849fe22a16edbae4641a612bc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-07 09:04:46 +0100

    Update and rename pgstat_.sql to pgstat.sql

--- postgres/scripts/pgstat.sql
+++ postgres/scripts/pgstat.sql
@@ -1,16 +1,17 @@
+-- Show WAL archiver status
 SELECT row_to_json(t)
 from (
 SELECT *, 'pg_stat_archiver' as tag from pg_stat_archiver
 ) t;
 
+-- Show replication status from master server
 SELECT row_to_json(t)
 from (
 SELECT *, 'pg_stat_replication' as tag from pg_stat_replication
 ) t;
 
--- Check the replication status from standby server
+-- Show replication status from standby server
 SELECT row_to_json(t)
 from (
 SELECT *, 'pg_stat_wal_receiver' as tag from pg_stat_wal_receiver
 ) t;
-
