20d89b4bf8a540a354bfea1a9aaea86b500c6f47
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-07 08:58:40 +0100

    Update

--- postgres/scripts/pglog.sh
+++ postgres/scripts/pglog.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
 yes "sudo -u postgres psql -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-yes "sudo -u postgres psql -t -f /scripts/pg_stat_archiver.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
+yes "sudo -u postgres psql -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
+
+yes "sudo -u postgres psql -h slave -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
+yes "sudo -u postgres psql -h slave -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
