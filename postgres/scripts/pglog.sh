42a0ef468ac242fd8ee54edcc905f4c8e42b9bc8
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-09 16:29:48 +0100

    Update pglog.sh

--- postgres/scripts/pglog.sh
+++ postgres/scripts/pglog.sh
@@ -1,7 +1,18 @@
 #/bin/bash
 
-yes "sudo -u postgres psql -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
+# Logging local
 yes "sudo -u postgres psql -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
 
-yes "sudo -u postgres psql -h slave -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
-yes "sudo -u postgres psql -h slave -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
+# Logging remote
+yes "sudo -u postgres psql -h primary -U replicator -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
+yes "sudo -u postgres psql -h secondary -U replicator -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
+
+
+
+
+# Stats local
+yes "sudo -u postgres psql -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
+
+# Stats remote
+yes "sudo -u postgres psql -h primary -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
+yes "sudo -u postgres psql -h secondary -U replicator -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60
