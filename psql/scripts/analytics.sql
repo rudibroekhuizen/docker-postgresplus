ef743a8fef4518819e4990c8f7e8a3789e1e96c0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-03 12:22:22 +0100

    Update analytics.sql

--- psql/scripts/analytics.sql
+++ psql/scripts/analytics.sql
@@ -1,17 +1,23 @@
+-- Get pg_log lines
+SELECT row_to_json(t)
+from (
+SELECT *, 'pg_log' as tag FROM pglog WHERE log_time > current_timestamp - interval '1 minutes'
+) t;
+
 -- Show WAL archiver status
 SELECT row_to_json(t)
 from (
-SELECT *, 'pg_stat_archiver' as tag from pg_stat_archiver
+SELECT *, 'pg_stat_archiver' as tag FROM pg_stat_archiver
 ) t;
 
 -- Show replication status from master server
 SELECT row_to_json(t)
 from (
-SELECT *, 'pg_stat_replication' as tag from pg_stat_replication
+SELECT *, 'pg_stat_replication' as tag FROM pg_stat_replication
 ) t;
 
 -- Show replication status from standby server
 SELECT row_to_json(t)
 from (
-SELECT *, 'pg_stat_wal_receiver' as tag from pg_stat_wal_receiver
+SELECT *, 'pg_stat_wal_receiver' as tag FROM pg_stat_wal_receiver
 ) t;
