bdaa7181d0fc2b5632c1cdaec6cb0566014e19ae
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-02-03 11:58:06 +0100

    CREATE EXTENSION pg_stat_statements

--- postgres/docker-entrypoint-initdb.d/10-init.sh
+++ postgres/docker-entrypoint-initdb.d/10-init.sh
@@ -9,6 +9,7 @@ echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.con
 
 # Create users
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+     CREATE EXTENSION pg_stat_statements;
      CREATE ROLE $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
      CREATE ROLE analytics WITH SUPERUSER PASSWORD 'postgres' LOGIN;
 EOSQL
