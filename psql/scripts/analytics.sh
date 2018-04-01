e2ab4f361fd6c1aa1993112fc80eae3dcf4ce386
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-04-01 19:54:53 +0200

    Update analytics.sh

--- psql/scripts/analytics.sh
+++ psql/scripts/analytics.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
-# Logging local
-yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/postgres -t -f /usr/local/bin/analytics.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60
-#yes "psql postgresql://analytics:postgres@postgres/postgres -t -f /usr/local/bin/analytics.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60
+# Logging postgres db
+yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/postgres -t -f /usr/local/bin/analytics_pg.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60 &&
 
-# Logging remote
-#yes "psql postgresql://analytics:postgres@primary/postgres -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-#yes "psql postgresql://analytics:postgres@secondary/postgres -t -f /scripts/pglog.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-
-
-
-# Stats local
-#yes "sudo -u postgres psql -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-
-# Stats remote
-#yes "psql postgresql://analytics:postgres@primary/postgres -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-#yes "psql postgresql://analytics:postgres@secondary/postgres -t -f /scripts/pgstat.sql | jq -c . >> /tmp/pglog.json" | parallel --jobs 1 --delay 60 &
-
-
-#sleep 1000
+# Logging specified db
+yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB} -t -f /usr/local/bin/analytics_db.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60
