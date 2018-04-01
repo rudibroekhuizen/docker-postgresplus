b954e9ad1a21bb9a5d2954465f5954e8b7687e01
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-04-01 20:05:41 +0200

    Update analytics.sh

--- psql/scripts/analytics.sh
+++ psql/scripts/analytics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Logging postgres db
-yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/postgres -t -f /usr/local/bin/analytics_pg.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60 &&
+yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/postgres -t -f /usr/local/bin/analytics_pg.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60 &
 
 # Logging specified db
 yes "psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB} -t -f /usr/local/bin/analytics_db.sql | jq -c . >> /tmp/analytics.json" | parallel --jobs 1 --delay 60
